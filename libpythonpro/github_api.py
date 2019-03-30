6a940f26ba093cb5939203f299ac9574f1f755f7
eduardo <egoncalvesfreitas@gmail.com>
2019-03-30 10:24:17 -0300

    Ajustes no flake8

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -7,11 +7,12 @@ def buscar_avatar(avatar):
         :param usuario: str com o nome de usuário no github
         :return:str com o link  do avatar
     """
-    #url  = 'https://api.github.com/users/{}'.format(avatar)
+    # url  = 'https://api.github.com/users/{}'.format(avatar)
     url = f'https://api.github.com/users/{avatar}'
     resp = requests.get(url)
-
     return resp.json()['avatar_url']
 
-if __name__  == '__main__':
+
+if __name__ == '__main__':
+
     print(buscar_avatar('renzon'))
